99b14dcd091731503acb3f14c416c7e6e86a7d48
Philipp Bucher <bucher@navigate.de>
2015-10-02 16:00:47 +0200

    Phantomjs update

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from 'buchix/tests/helpers/start-app';
-import appVersion from 'buchix/initializers/app-version';
 
 module('Acceptance | login', {
   beforeEach: function () {
